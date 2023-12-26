3e556ead3f7a1401a6866e9594b30a87f53a8fce
manoj balaga <manojbalaga6268@gmail.com>
2023-12-26 20:54:48 +0530

    added the comment in a files

--- src/com/first/Divisibleby3num.java
+++ src/com/first/Divisibleby3num.java
@@ -3,6 +3,7 @@ package com.first;
 public class Divisibleby3num {
 	public static void main(String[] args) {
 		int a = 30;
+		//comment
 		if (a % 3 ==0 &&  a % 5 ==0 && a % 10==0) {
 			System.out.println("a is divisible by all 3 numbers");
 		} else {
