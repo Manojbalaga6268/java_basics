44ccf0155c428bab98350707191bd7b24b9c52e4
manoj balaga <manojbalaga6268@gmail.com>
2023-12-27 16:17:09 +0530

    commit

--- src/com/arrays/array.java
+++ src/com/arrays/array.java
@@ -2,6 +2,7 @@ package com.arrays;
 
 public class array {
 	public static void main(String[] args) {
+		//commit
 		int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
 		for (int i = 0; i < array.length; i++) {
